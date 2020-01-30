6d6d54225a8ce2bd4009056a92c2ab3710801da5
fridlund <friddelund@gmail.com>
2020-01-30 17:28:26 +0100

    Init

---
+++ src/main/java/iv1201/group1/applications/recruitment/DemoApplication.java
@@ -0,0 +1,13 @@
+package iv1201.group1.applications.recruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iv1201/group1/applications/recruitment/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package iv1201.group1.applications.recruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
