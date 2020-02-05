cc5fc379260772c9f05d4f83c0dc821253c6160c
fridlund <friddelund@gmail.com>
2020-02-05 17:46:22 +0100

    idfk

--- src/main/java/iv1201/group1/applications/recruitment/DemoApplication.java
+++ src/main/java/iv1201/group1/applications/recruitment/DemoApplication.java
@@ -2,11 +2,19 @@ package iv1201.group1.applications.recruitment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
