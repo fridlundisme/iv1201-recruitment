d0d8606812f082eec6517929025101681190e3c5
fridlund <friddelund@gmail.com>
2020-02-10 11:01:44 +0100

    Database test

---
+++ src/main/java/iv1201/group1/applications/recruitment/controller/Db.java
@@ -0,0 +1,13 @@
+package iv1201.group1.applications.recruitment.controller;
+
+public class Db{
+    private String sel;
+
+    public String getSel(){
+        return sel;
+    }
+
+    public void setSel(String s){
+        this.sel = s;
+    }
+}
--- src/main/java/iv1201/group1/applications/recruitment/controller/LoginController.java
+++ src/main/java/iv1201/group1/applications/recruitment/controller/LoginController.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -26,10 +27,12 @@ public class LoginController {
     ModelNotinuse model = new ModelNotinuse();
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login() {
+    public String login(Model model, @ModelAttribute Db db) {
+        model.addAttribute("db", new Db());
         String s = Integration.select();
+        db.setSel(s);
 
-        return "Hehjhh";
+        return "test";
     }
     // @RequestMapping(value = "/postLogin", method = RequestMethod.POST)
     // public String postLogin(Model model, HttpSession session) {
