3930a268428ce3919aae0639c22466bbddff6dff
owilen@kth.se <owilen@kth.se>
2020-02-12 11:53:17 +0100

    Nu funkar skiten

--- src/main/java/iv1201/group1/applications/recruitment/controller/LoginController.java
+++ src/main/java/iv1201/group1/applications/recruitment/controller/LoginController.java
@@ -11,6 +11,7 @@ import iv1201.group1.applications.recruitment.model.UserJpaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -20,7 +21,7 @@ import java.util.*;
 
 import javax.servlet.http.HttpSession;
 
-@RestController
+@Controller
 public class LoginController {
 
     @GetMapping("/login")
