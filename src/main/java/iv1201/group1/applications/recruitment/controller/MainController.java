4986b80ab5d96bd421bae7aba878108f87b0d210
fridlund <friddelund@gmail.com>
2020-02-14 11:48:28 +0100

    Register form init

---
+++ src/main/java/iv1201/group1/applications/recruitment/controller/MainController.java
@@ -0,0 +1,18 @@
+package iv1201.group1.applications.recruitment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController{
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+}
