3b35c372ee96a1f70222c8c87f5b61822fc6862e
Oh_liver <owilen@kth.se>
2020-02-17 16:55:41 +0100

    Revert "Oliver merge to master"

--- src/main/java/iv1201/group1/applications/recruitment/config/WebSecurityConfig.java
+++ src/main/java/iv1201/group1/applications/recruitment/config/WebSecurityConfig.java
@@ -1,13 +1,9 @@
 package iv1201.group1.applications.recruitment.config;
 
-import iv1201.group1.applications.recruitment.service.UserDetailsServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -21,40 +17,65 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import iv1201.group1.applications.recruitment.controller.LoginController;
 @Configuration
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Qualifier("userDetailsServiceImpl")
-    @Autowired
-    public UserDetailsService userDetailsService;
+
+    /*@Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }*/
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/resources/**", "/index", "/registration")
-                    .permitAll()
-                    .anyRequest().authenticated()
-                    .and()
+                //.antMatchers("/", "/index").permitAll()
+                .anyRequest().authenticated()
+                .and()
                 .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .and()
-                .logout()
-                    .permitAll();
+                .loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .permitAll()
+                .loginProcessingUrl("/login")
+                .successForwardUrl("/postLogin")
+                .and()
+                .logout().logoutUrl("/doLogout")
+                .permitAll();
     }
 
-    @Bean
-    public AuthenticationManager customAuthenticationManager() throws Exception {
-        return authenticationManager();
-    }
+    /*@Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        new LoginController controller = new LoginController();
+        LoginDetails loginDetails = new LoginDetails(.username, .password);
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
-    }
+        if (controller.tryLogin(loginDetails)) {
+
+            UserDetails user =
+                    User.withDefaultPasswordEncoder()
+                            .roles("USER")
+                            .build();
+
+            return new InMemoryUserDetailsManager(user);
+        }
+    }*/
 }
