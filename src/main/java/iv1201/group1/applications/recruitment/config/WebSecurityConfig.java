54450b2b84bee315ab018a1b36634d6f4f6f45ac
fridlund <friddelund@gmail.com>
2020-02-17 16:32:25 +0100

    Added registration form

--- src/main/java/iv1201/group1/applications/recruitment/config/WebSecurityConfig.java
+++ src/main/java/iv1201/group1/applications/recruitment/config/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/register", "/index").permitAll()
+                .antMatchers("/","/register", "/index","/css/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
