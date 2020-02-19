ed9c2f096a300fca0f135f16640332707b3d6be0
owilen@kth.se <owilen@kth.se>
2020-02-19 13:49:24 +0100

    Fixed persistance mapping and moved application-properties

--- src/main/client/scripts/makedb.sql
+++ src/main/client/scripts/makedb.sql
@@ -22,13 +22,13 @@ CREATE TABLE webapplication.person (
     ssn VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role_id BIGINT REFERENCES webapplication.role (role_id) ON DELETE CASCADE,
+    role_id INTEGER REFERENCES webapplication.role (role_id) ON DELETE CASCADE NOT NULL,
     username VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE webapplication.availability (
     availability_id SERIAL PRIMARY KEY,
-    person_id BIGINT REFERENCES webapplication.person (person_id) ON DELETE CASCADE,
+    person_id INTEGER REFERENCES webapplication.person (person_id) ON DELETE CASCADE NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL
 );
@@ -40,7 +40,7 @@ CREATE TABLE webapplication.competence (
 
 CREATE TABLE webapplication.competence_profile (
     competence_profile_id SERIAL PRIMARY KEY,
-    person_id BIGINT REFERENCES webapplication.person (person_id) ON DELETE CASCADE,
-    competence_id BIGINT REFERENCES webapplication.competence (competence_id) ON DELETE CASCADE,
+    person_id INTEGER REFERENCES webapplication.person (person_id) ON DELETE CASCADE NOT NULL,
+    competence_id INTEGER REFERENCES webapplication.competence (competence_id) ON DELETE CASCADE NOT NULL,
     years_of_experience NUMERIC(4,2) NOT NULL
 );
