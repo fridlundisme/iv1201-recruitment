b7a36afc1fc916e5c08ce3c758c47fc5448c7670
fridlund <friddelund@gmail.com>
2020-02-27 13:59:12 +0100

    Changed name to main

--- src/main/java/iv1201/group1/applications/recruitment/Main.java
+++ src/main/java/iv1201/group1/applications/recruitment/Main.java
@@ -2,14 +2,11 @@ package iv1201.group1.applications.recruitment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
 
 @Controller
 @SpringBootApplication
 public class Main {
-
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
