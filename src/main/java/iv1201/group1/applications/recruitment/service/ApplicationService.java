3447d04e14db430b11a370d70da48c08b77d7e21
owilen@kth.se <owilen@kth.se>
2020-02-28 13:34:33 +0100

    added classes

---
+++ src/main/java/iv1201/group1/applications/recruitment/service/ApplicationService.java
@@ -0,0 +1,11 @@
+package iv1201.group1.applications.recruitment.service;
+
+import iv1201.group1.applications.recruitment.domain.Competence;
+
+import java.util.List;
+
+public interface ApplicationService {
+   List<Competence> getCompetenceList();
+
+
+}
---
+++ src/main/java/iv1201/group1/applications/recruitment/service/ApplicationServiceImpl.java
@@ -0,0 +1,27 @@
+package iv1201.group1.applications.recruitment.service;
+
+import iv1201.group1.applications.recruitment.domain.Competence;
+import iv1201.group1.applications.recruitment.model.AvailabilityJpaRepository;
+import iv1201.group1.applications.recruitment.model.CompetenceJpaRepository;
+import iv1201.group1.applications.recruitment.model.PersonJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public class ApplicationServiceImpl implements ApplicationService {
+   @Autowired
+   private CompetenceJpaRepository competenceJpaRepository;
+
+   @Autowired
+   private PersonJpaRepository personJpaRepository;
+
+   @Autowired
+   private AvailabilityJpaRepository availabilityJpaRepository;
+
+   @Override
+   @Transactional(readOnly = true)
+   public List<Competence> getCompetenceList() {
+      return competenceJpaRepository.findAll();
+   }
+}
