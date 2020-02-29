b673f57165817fb0bbf2f0c7975d72bbc64aa326
owilen@kth.se <owilen@kth.se>
2020-02-29 12:35:12 +0100

    fixed bugs

--- src/main/java/iv1201/group1/applications/recruitment/service/ApplicationService.java
+++ src/main/java/iv1201/group1/applications/recruitment/service/ApplicationService.java
@@ -1,11 +1,16 @@
 package iv1201.group1.applications.recruitment.service;
 
+import iv1201.group1.applications.recruitment.domain.Availability;
 import iv1201.group1.applications.recruitment.domain.Competence;
+import iv1201.group1.applications.recruitment.domain.CompetenceProfile;
+import iv1201.group1.applications.recruitment.domain.Person;
 
 import java.util.List;
 
 public interface ApplicationService {
    List<Competence> getCompetenceList();
 
+   void setCompetenceProfile(Person person, Competence competence, CompetenceProfile competenceProfile);
 
+   void setAvailability(Person person, Availability availability);
 }
--- src/main/java/iv1201/group1/applications/recruitment/service/ApplicationServiceImpl.java
+++ src/main/java/iv1201/group1/applications/recruitment/service/ApplicationServiceImpl.java
@@ -1,8 +1,12 @@
 package iv1201.group1.applications.recruitment.service;
 
+import iv1201.group1.applications.recruitment.domain.Availability;
 import iv1201.group1.applications.recruitment.domain.Competence;
+import iv1201.group1.applications.recruitment.domain.CompetenceProfile;
+import iv1201.group1.applications.recruitment.domain.Person;
 import iv1201.group1.applications.recruitment.model.AvailabilityJpaRepository;
 import iv1201.group1.applications.recruitment.model.CompetenceJpaRepository;
+import iv1201.group1.applications.recruitment.model.CompetenceProfileJpaRepository;
 import iv1201.group1.applications.recruitment.model.PersonJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,9 +25,27 @@ public class ApplicationServiceImpl implements ApplicationService {
    @Autowired
    private AvailabilityJpaRepository availabilityJpaRepository;
 
+   @Autowired
+   private CompetenceProfileJpaRepository competenceProfileJpaRepository;
+
    @Override
    @Transactional(readOnly = true)
    public List<Competence> getCompetenceList() {
       return competenceJpaRepository.findAll();
+   }
+
+   @Override
+   @Transactional
+   public void setCompetenceProfile(Person person, Competence competence, CompetenceProfile competenceProfile) {
+      competenceProfile.setPerson(person);
+      competenceProfile.setCompetence(competence);
+      competenceProfileJpaRepository.save(competenceProfile);
+   }
+
+   @Override
+   @Transactional
+   public void setAvailability(Person person, Availability availability) {
+      availability.setPerson(person);
+      availabilityJpaRepository.save(availability);
    }
 }
