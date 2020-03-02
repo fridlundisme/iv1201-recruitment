ad1985f8e4748853a93038db3179eb2d93b4fd97
ssoderst <ssoderst@kth.se>
2020-03-02 19:26:32 +0100

    working on the recruiter view

--- src/main/java/iv1201/group1/applications/recruitment/controller/RecruiterController.java
+++ src/main/java/iv1201/group1/applications/recruitment/controller/RecruiterController.java
@@ -5,21 +5,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+/**
+ * Class for handling the recruiter view and its functionality.
+ */
 @Controller
 public class RecruiterController {
 
 
-
+   /**
+    * Function for accepting the applicant
+    */
    //Hire Applicant
    public void hireApplicant() {
 
    }
 
+   /**
+    * Function for denying the applicant
+    */
    //deny Applicant
    public void denyApplicant() {
 
    }
 
+   /**
+    * Function for getting the page that views one specific application.
+    * @param person is the applicant.
+    * @return returns the requested view.
+    */
    //show detailed view of chosen applicant
    @PostMapping("/recruit")
    public String viewApplicant(Person person) {
